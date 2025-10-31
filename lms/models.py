062cfe6be257647113b82c75520506b0f89ec966
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-10-31 22:12:20 +0300

    add lms app

---
+++ lms/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    image = models.ImageField(blank=True, null=True, upload_to='images/', verbose_name='Превью')
+    video_url = models.URLField(blank=True, null=True, verbose_name='Ссылка на видео')
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    image = models.ImageField(blank=True, null=True, upload_to='images/', verbose_name='Превью')
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name= 'lessons', verbose_name='Курс')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
