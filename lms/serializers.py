771888ac0500b8b3c569c28e837b61a1e7818da2
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-11-02 15:59:38 +0300

    update serializers

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,8 +3,15 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField(read_only=True)
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lesson_count(self, obj):
         if obj.lessons.all():
@@ -14,10 +21,4 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = '__all__'
