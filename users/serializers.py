f6631989daa1ecc9962ed151b915aba3d77e3a90
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-11-02 16:07:39 +0300

    update serializers

--- users/serializers.py
+++ users/serializers.py
@@ -3,16 +3,17 @@ from rest_framework import serializers
 from .models import CustomUser, Payment
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = [
-            'first_name', 'last_name', 'phone_number', 'avatar', 'country',
-        ]
-
-
 class PaymentSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'first_name', 'last_name', 'phone_number', 'avatar', 'country', 'payments',
+        ]
