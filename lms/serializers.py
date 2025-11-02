86aa710496f743a4bc460ff1dae001af7be151b1
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-11-02 14:51:32 +0300

    update CourseSerializer (add get_lesson_count funk)

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,9 +4,18 @@ from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField(read_only=True)
+
+    def get_lesson_count(self, obj):
+        if obj.lessons.all():
+            return obj.lessons.all().count()
+        else:
+            return 0
+
     class Meta:
         model = Course
         fields = '__all__'
+
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
