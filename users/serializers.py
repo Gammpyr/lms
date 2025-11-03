67e2eb20eb27a8dce549c7546fc31e4c63f143f7
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-11-03 18:52:21 +0300

    update users.serializers (add Payment validate)

--- users/serializers.py
+++ users/serializers.py
@@ -8,6 +8,25 @@ class PaymentSerializer(serializers.ModelSerializer):
         model = Payment
         fields = '__all__'
 
+    def validate_payment_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Сумма платежа должна быть больше 0.")
+        return value
+
+    def validate(self, data):
+        paid_course = data.get('paid_course')
+        paid_lesson = data.get('paid_lesson')
+
+        if not paid_course and not paid_lesson:
+            raise serializers.ValidationError(
+                "Укажите, за что вы хотите оплатить. За курс или урок."
+            )
+        if paid_course and paid_lesson:
+            raise serializers.ValidationError(
+                "Можно оплатить либо за курс, либо за урок."
+            )
+        return data
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     payments = PaymentSerializer(many=True, read_only=True)
