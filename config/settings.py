f56871474fcab8d58060306bc7fddf75b6f80baf
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-11-05 22:10:59 +0300

    install djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_filters',
 
     'lms',
@@ -50,6 +51,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
 }
 
 MIDDLEWARE = [
