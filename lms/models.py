5ba5359797974429c2e06d7de77ec3bf14c79776
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-11-06 00:38:34 +0300

    update models.py (add owner fields)

--- lms/models.py
+++ lms/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Course(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(blank=True, verbose_name='Описание')
     image = models.ImageField(blank=True, null=True, upload_to='images/', verbose_name='Превью')
     video_url = models.URLField(blank=True, null=True, verbose_name='Ссылка на видео')
-
+    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, blank=True, null=True,
+                              related_name='courses', verbose_name='Владелец')
 
     def __str__(self):
         return self.name
@@ -19,8 +21,10 @@ class Lesson(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     image = models.ImageField(blank=True, null=True, upload_to='images/', verbose_name='Превью')
     description = models.TextField(blank=True, verbose_name='Описание')
+    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, blank=True, null=True,
+                              related_name='lessons', verbose_name='Владелец')
 
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name= 'lessons', verbose_name='Курс')
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name='course_lessons', verbose_name='Курс')
 
     def __str__(self):
         return self.name
